ac2d211f66241159ee7f2e8b59b8e05b32e86609
Kshitij Subedi <2subedikshitij@gmail.com>
2026-06-13 09:39:02 +0545

    Update nepali-calendar to 2.5.5

--- Casks/nepali-calendar.rb
+++ Casks/nepali-calendar.rb
@@ -1,10 +1,10 @@
 cask "nepali-calendar" do
-  version "2.5.4,35"
-  sha256 "2808e9212a1f03f2b8046db3b7355c2d6137603f9062cec0b7ce3793bf372146"
+  version "2.5.5,36"
+  sha256 "82a22269c88fa4701b6fa43bb2faf8239d28b96e863fa54d26232f2f9ffbcd46"
 
   url "https://github.com/kshitijsubedi/nepali-calendar-mac-updates/raw/refs/heads/main/versions/nepali-calendar-mac-#{version.csv.first}-#{version.csv.second}.dmg"
   name "Nepali Calendar"
-  desc "Nepali calendar and date converter widget for macOS"
+  desc "Nepali Calendar and Date converter Menubar app & widget for macOS"
   homepage "https://github.com/kshitijsubedi/nepali-calendar-widgets"
 
   depends_on macos: ">= :ventura"
